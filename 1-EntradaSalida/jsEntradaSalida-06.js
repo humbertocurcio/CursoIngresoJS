7306ae354e9aa0b24281560d462a51685a938a7f
Humberto Curcio <curciohumberto@gmail.com>
2023-02-01 15:06:55 +0100

    ejercicio parseInt

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,20 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numUnoTexto;
+	let numUno;
+	let numDosTexto;
+	let numDos;
+	let mensaje;
+
+
+	numUnoTexto = document.getElementById ("txtIdNumeroUno").value;
+	numDosTexto = document.getElementById ("txtIdNumeroDos").value;
+
+	numUno = parseInt (numUnoTexto);
+	numDos = parseInt (numDosTexto);
+	mensaje = "La suma es " + numUno + numDos;
+
+	alert(mensaje);
 }
 
