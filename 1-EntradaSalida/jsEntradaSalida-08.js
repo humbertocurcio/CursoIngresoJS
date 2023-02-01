7f065b480c6ad51e2a4f70c02212a3dd318ff7d1
Humberto Curcio <curciohumberto@gmail.com>
2023-02-01 16:38:30 +0100

    ejercicio 8 de sacar resto resuelto

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,19 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	let numDividendoTexto
+	let numDividendo
+	let numDivisorTexto
+	let numDivisor
+	let mensaje
+
+	numDividendoTexto= document.getElementById ("txtIdNumeroDividendo").value;
+	numDivisorTexto = document.getElementById ("txtIdNumeroDivisor").value;
+
+	numDividendo = parseInt (numDividendoTexto);
+	numDivisor = parseInt (numDivisorTexto);
+	
+	mensaje = "El resto es " + numDividendo % numDivisor;
+
+	alert(mensaje);
 }
