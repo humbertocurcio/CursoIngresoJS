96b993b2fbd41f00a69af8e42ada8d94e272d437
Humberto Curcio <curciohumberto@gmail.com>
2023-02-06 20:08:38 +0100

    ejercicio IF 4 resuelto

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,22 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//tomo la edad 
+	
+	let edad
+	
+	edad = parseInt(document.getElementById ("txtIdEdad").value);
+
+	if(edad >=13)
+	{
+		if(edad <=17)
+		{
+			alert("la persona es adolescente");
+		}
+	}
+
+
+
+	
 
 
 }//FIN DE LA FUNCIÓN
