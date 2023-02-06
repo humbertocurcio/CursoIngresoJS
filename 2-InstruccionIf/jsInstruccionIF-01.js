e6dade5fc06ff1dbe7b1ebc1996fe6426bf932ea
Humberto Curcio <curciohumberto@gmail.com>
2023-02-06 19:12:37 +0100

    ejercicio IF 1 resuelto

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//declaro la variable
+
+	let edad;
+
+	//asigno valor a la variable
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	//condicion
+
+	if(edad == 15)
+	{
+		alert("niña bonita");
+		
+		
+	}
+	
+
+	
 
 }//FIN DE LA FUNCIÓN
