787d3e8224c545ae2da438295f50d2432ced3847
Humberto Curcio <curciohumberto@gmail.com>
2023-02-06 19:27:53 +0100

    ejercicio IF 3 resuelto

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -10,10 +10,10 @@ function mostrar()
 	{
 		alert("la persona es mayor de edad");
 	}
-	
+
 	if(edad < 18)
 	{
-		alert("la persona es menor de edad");
+		alert("la persona es menor de edad"); 
 	}
 	
 
