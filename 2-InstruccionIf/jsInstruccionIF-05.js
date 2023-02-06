96e9946a56ef8ed0d3fe6e5bd9d717579f60ab89
Humberto Curcio <curciohumberto@gmail.com>
2023-02-06 21:11:28 +0100

    solucion de IF 5 a traves de negacion

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+
+	let edad
+	
+	edad = parseInt(document.getElementById ("txtIdEdad").value);
+
+	if( !(edad >= 13  && edad <= 17) )
+	{
+		alert("usted NO es adolecente");
+
+	}
+
+
 
 }//FIN DE LA FUNCIÓN
