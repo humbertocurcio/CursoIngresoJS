e99eee569b4789478124b651b46e40d8c9e5bd42
Humberto Curcio <curciohumberto@gmail.com>
2023-02-06 19:27:02 +0100

    ejercicio IF 3

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+
+	let edad
+
+	edad = parseInt(document.getElementById ("txtIdEdad").value);
+
+	if(edad >= 18)
+	{
+		alert("la persona es mayor de edad");
+	}
+	
+	if(edad < 18)
+	{
+		alert("la persona es menor de edad");
+	}
+	
 
 }//FIN DE LA FUNCIÓN
