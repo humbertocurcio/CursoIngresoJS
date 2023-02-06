3e32af51aedd4f55eb8ba65c021101230843aca9
Humberto Curcio <curciohumberto@gmail.com>
2023-02-06 19:16:40 +0100

    ejercicio IF 2 resuelto

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,17 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+
+	let edad
+
+	edad = parseInt (document.getElementById("txtIdEdad").value);
+
+	if (edad >= 18)
+	{
+		alert("la persona es mayor de edad");
+
+	}
+	
 	
 
 }//FIN DE LA FUNCIÓN
