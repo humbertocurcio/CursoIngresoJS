cb13ac8ab59955e0490c64e3ca60347d578d93fb
Humberto Curcio <curciohumberto@gmail.com>
2023-02-07 21:14:56 +0100

    ejercicio 9 resuelto

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	
+	let numAleat;
+	const NMAX = 10
+	const NMIN = 1
+
+	numAleat = Math.round( Math.random() * (NMAX - NMIN) + NMIN);
+
+	alert(numAleat);	
+
+
+	
 
 }//FIN DE LA FUNCIÓN
