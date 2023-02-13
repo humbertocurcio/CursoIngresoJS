728c42505c5e01a5cbc81035951dd2f8a693e790
Humberto Curcio <curciohumberto@gmail.com>
2023-02-13 20:12:20 +0100

    ejercicio 1 SWITCH resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,31 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesAnio;
 
+	mesAnio = document.getElementById("txtIdMes").value;
+
+	switch(mesAnio)
+	{
+		case "Enero":
+			alert("Que comiencen bien el año");
+			break;
+		
+		case "Marzo":
+			alert("A clases!!!");
+			break;
+
+		case "Julio":
+			alert("Se vienen las vacaciones");
+			break;
+
+		case "Diciembre":
+			alert("Felices fiestas!!!");
+			break;
+		default :
+			break;
+	}
+
+	
 
 
 }//FIN DE LA FUNCIÓN
