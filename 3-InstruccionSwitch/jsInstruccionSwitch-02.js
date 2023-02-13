27163e9514db373d00e6473915badd45f92081a3
Humberto Curcio <curciohumberto@gmail.com>
2023-02-13 20:01:50 +0100

    ejercicio 2 SWITCH resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,8 +1,27 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesAnio;
+
+	mesAnio = document.getElementById("txtIdMes").value;
+
+	switch(mesAnio)
+	{
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Mayo":
+		case "Abril":
+		case "Junio":
+			alert("Falta para el invierno");
+			break;
+		case "Julio":
+		case "Agosto":
+			alert("abrigate que hace frio");
+			break;
+		default:
+			alert("ya pasamos el frio, ahora el calor");
+	}
+		
 
 
 
