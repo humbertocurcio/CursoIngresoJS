e378359b0f08c946d6d272a28055fd297a4730f3
Humberto Curcio <curciohumberto@gmail.com>
2023-02-14 18:31:21 +0100

    ejercicio 7 SWITCH resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,22 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
 
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch(destino)
+	{
+		case "Bariloche":
+			alert("està en el oeste");
+			break;
+		case "Cataratas":
+			alert("està al norte");
+			break;
+		case "Mar del Plata":
+			alert("està al este");
+			break;
+		case "Ushuaia":
+			alert("està al sur");
+			break;
+	}
 }//FIN DE LA FUNCIÓN
