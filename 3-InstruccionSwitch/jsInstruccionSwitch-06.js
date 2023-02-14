c71e5a12b1210d609694eac03678959dced10627
Humberto Curcio <curciohumberto@gmail.com>
2023-02-14 18:32:49 +0100

    ejercicio 7 SWITCH resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -4,7 +4,43 @@ function mostrar()
 
 	horaDelDia = parseInt(document.getElementById("txtIdHora").value);
 
-	switch(horaDelDia)
+	if (horaDelDia >= 7 && horaDelDia <= 11)
+	{
+		alert("Es de mañana");
+	}
+	else if (horaDelDia >= 12 && horaDelDia <= 19)
+	{
+		alert("Es de tarde");
+	}
+	else if ((horaDelDia >= 20 && horaDelDia <= 24) || (horaDelDia >= 0 && horaDelDia <= 6))
+	{
+		alert("Es de noche")
+	}
+	else
+	{
+		alert("Esa no es una hora valida");
+	}
+	document.getElementById("txtIdHora").value = "";
+
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*switch(horaDelDia)
 	{
 		case 7:
 		case 8:
@@ -41,6 +77,6 @@ function mostrar()
 			alert("La hora no existe");
 			break;
 
-	}
+	}*/
 
 }//FIN DE LA FUNCIÓN
