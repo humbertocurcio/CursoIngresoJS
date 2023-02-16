dd920b57aa04f5d70c47b729891b6784316b9157
Humberto Curcio <curciohumberto@gmail.com>
2023-02-16 19:31:54 +0100

    ejercicio 4 WHILE resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,14 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	let numeroIngresado;
+
+	numeroIngresado =parseInt(prompt("ingrese un número entre 0 y 9"));
+
+	while(numeroIngresado < 0 || numeroIngresado > 9)
+	{
+		numeroIngresado =parseInt(prompt("ingrese un número entre 0 y 9"));
+	}
+	document.getElementById("txtIdNumero").value = numeroIngresado;
 	
 }//FIN DE LA FUNCIÓN
