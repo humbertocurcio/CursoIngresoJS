3dae14f8df52ef5f39a7d84c3e213744becfe253
Humberto Curcio <curciohumberto@gmail.com>
2023-02-16 21:33:11 +0100

    ejercicio 7 WHILE resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -4,15 +4,30 @@ e informar la suma acumulada y el promedio.
 */
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var respuesta;
-	contador=0;
-	acumulador=0;
-	respuesta='si';
+	let contador;
+	let acumulador;
+	let respuesta;
+	let numeroIngresado;
+
+	contador = 0;
+	acumulador = 0;
+	respuesta = `S`;
+
+	
+
+	while (respuesta == `S`)
+	{
+		numeroIngresado = parseFloat(prompt("Ingrese un numero"));
+		acumulador = acumulador + numeroIngresado;
+		contador = contador + 1;
+		respuesta = prompt("desea ingresar otro numero? S/N");
+	}
+
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = acumulador / contador;
 
 
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/contador;
+
+	
 
 }//FIN DE LA FUNCIÓN
