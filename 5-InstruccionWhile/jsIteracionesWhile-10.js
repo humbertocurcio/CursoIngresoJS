65cdcec71c581df0fde14c8263eedbb323ca0342
Humberto Curcio <curciohumberto@gmail.com>
2023-02-23 00:42:52 +0100

    ejercicio 10 WHILE resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -12,21 +12,71 @@ hasta que el usuario quiera, mostrar:
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
 
-	respuesta="si";
+	let numero;
+	let promPositivos;
+	let promNegativos;
+	let diferenciaPosNeg;
+	let contadorPositivos = 0;
+	let contadorNegativos = 0;
+	let contadorCeros = 0;
+	let contadorPares = 0;
+	let acumuladorPositivos = 0;
+	let acumuladorNegativos = 0;
+	let respuesta = `s`;
 
-	while(respuesta=="si")
+
+	while(respuesta == `s`)
 	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		numero = parseFloat(prompt("ingrese un nùmero: "));
+
+		if(numero > 0)
+		{
+			acumuladorPositivos += numero;
+			contadorPositivos ++;
+
+		}else if(numero == 0)
+		{
+			contadorCeros ++;
+
+		}else
+		{
+			acumuladorNegativos += numero;
+			contadorNegativos ++;
+		}
+		if(numero % 2 == 0)
+		{
+			contadorPares ++;
+		}
+
+		respuesta = prompt("desea ingresar otro nùmero? ");
+	}
+
+	promPositivos = acumuladorPositivos / contadorPositivos;
+
+	promNegativos = acumuladorNegativos / contadorNegativos;
+
+	diferenciaPosNeg = contadorPositivos - contadorNegativos;
+
+	document.write("La suma de negativos es " + acumuladorNegativos); 
+	document.write("La suma de positivos es " + acumuladorPositivos); 
+	document.write("La cantidad de positivos es " + contadorPositivos); 
+	document.write("La cantidad de negativos es " + contadorNegativos);
+	document.write("La cantidad de ceros es " + contadorCeros); 
+	document.write("La cantidad de numeros pares " + contadorPares); 
+	document.write("El promedio de los positivos es  " + promPositivos); 
+	document.write("El promedio de los negativos es " + promNegativos); 
+	document.write("La diferencia entre positivos y negativos es  " + diferenciaPosNeg);
 
 
 
-	//"<br>" salto de linea
-	document.write("la suma de negativos es :"+sumaNegativos);
+	
+
+
+
+
+
+
+	
+
 }//FIN DE LA FUNCIÓN
