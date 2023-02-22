b65d3e186ed725f3995660a6053c14e22e9e15d4
Humberto Curcio <curciohumberto@gmail.com>
2023-02-22 22:42:57 +0100

    ejercicio 9 WHILE resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -2,20 +2,41 @@
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+{	
+	let maximo;
+	let minimo;
+	let numero;
+	let respuesta;
+	let contador;
+
+	contador = 0;
+	respuesta = `s`;
+
+	while(respuesta == `s`)
 	{
+		numero = parseFloat(prompt("Ingrese un nùmero; "));
+		if(contador == 0)
+		{
+			maximo = numero;
+			minimo = numero;
+			contador = 1;
+		}
+		if(numero > maximo)
+		{
+			maximo = numero;
+		}
+		else if(numero < minimo)
+		{
+			minimo = numero;
+		}
 		
-		respuesta=prompt("desea continuar?");
+
+		respuesta = prompt("desea ingresar otro nùmero? s/n");
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+
+	
+
+	document.getElementById("txtIdMaximo").value = maximo;
+	document.getElementById("txtIdMinimo").value = minimo;
+
 }//FIN DE LA FUNCIÓN
